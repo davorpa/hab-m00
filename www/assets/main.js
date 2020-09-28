d909ba817367ab5f8859b3aead50a4bb2d8016b4
David Ordás <katelme@gmail.com>
2020-09-28 21:21:56 +0200

    Nomenclatura de variables

--- www/assets/main.js
+++ www/assets/main.js
@@ -2,4 +2,21 @@
 
 console.log("Hola mundo!");
 
+
+// Podemos utilizar números pero no en la primera posición.
+let numero_1 = 3;
+
+// Esta nomenclatura no es correcta.
+// let 1numero = 3;
+
+// Debemos evitar el uso de acentos.
+let numero_2 = 4;
+
+// Mala idea.
+let número_3 = 6;
+
+// Debemos evitar el uso de la "ñ".
+const patasArana = 8;
+
+
 }).call(this);
